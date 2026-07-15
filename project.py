3fdb033c9a5899c87149669c375a9e8ab42c749e
plus2content2026-afk <plus2content2026@gmail.com>
2026-07-15 13:34:44 +0530

    Update project.py

--- project.py
+++ project.py
@@ -4,10 +4,9 @@ con = m.connect(host="localhost",user="root",passwd="student")
 c=con.cursor()
 ctk.set_appearance_mode("Dark")
 ctk.set_default_color_theme("dark-blue")
-
 app = ctk.CTk()
 app.geometry("700x500")
-app.title('Database Entry')
+app.title(' Timetable Entry')
 app.grid_columnconfigure(0, weight=1)
 app.grid_columnconfigure(1, weight=1)
 label = ctk.CTkLabel(app,text="Welcome to Timetable Entry", font=('Arial',23))
@@ -16,24 +15,40 @@ entry = ctk.CTkEntry(app,placeholder_text="Type in your Name",state="normal",wid
 entry.grid(row=1,column=0,padx=30,pady=20,sticky="e")
 entry1 = ctk.CTkEntry(app,placeholder_text="Type in your Code",state="normal",width=280)
 entry1.grid(row=1,column=1,padx=30,pady=20,sticky="w")
-label2 = ctk.CTkLabel(app,text="")
-label2.grid(column=0,row=3,padx=10,pady=10,columnspan=3)
-def insertval():
+def createtable():
     name= entry.get()
     global code
     code = entry1.get()
     c.execute("USE user1")
-    c.execute("CREATE TABLE IF NOT EXISTS {}(Emp_id int, Emp_name varchar(20),Gender char(1),Salary int)".format(code))
-    c.execute(" INSERT INTO emp1 values({},'{}','{}',{})".format(eid,name,eg,es))
-    con.commit()
-    c.execute("SELECT * from {code}")
-    records = c.fetchall()
-    label2.configure(text=records)
-    entry.delete(0, 'end')A
+    c.execute(f"""
+        CREATE TABLE IF NOT EXISTS {code} (
+            Day VARCHAR(8), 
+            P1 VARCHAR(10), 
+            P2 VARCHAR(10), 
+            P3 VARCHAR(10), 
+            P4 VARCHAR(10), 
+            P5 VARCHAR(10), 
+            P6 VARCHAR(10), 
+            P7 VARCHAR(10), 
+            P8 VARCHAR(10)
+            )
+        """)
+
+    entry.delete(0, 'end')
     entry1.delete(0, 'end')
-    entry2.delete(0,'end')
-    entry3.delete(0,'end')
-    print("Table modified")
-button = ctk.CTkButton(app, text="Enter", command=insertval)
+    insertval()
+def insertval():
+    tablewin = ctk.CTkToplevel(app)
+    tablewin.title("Create/Edit")
+    tablewin.geometry("450x200")
+    cb = ctk.CTkButton(tablewin,text="Create Timetable",command=createtable)
+    eb = ctk.CTkButton(tablewin,text="Show existing table",command=showtable)
+    cb.grid(row=1,column=0,padx=30,pady=20,sticky="e")
+    eb.grid(row=1,column=2,padx=30,pady=20,sticky="w")
+    tablewin.focus()
+    tablewin.lift()
+def createtable():
+    
+button = ctk.CTkButton(app, text="Enter", command=createtable)
 button.grid(column=0,row=4,pady=10,padx=10,columnspan=3)
 app.mainloop()
